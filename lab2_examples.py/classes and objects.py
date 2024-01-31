19fb20a21545a55ccd91791420b9954d9b1c2926
DaryiaK26 <kapuzayeva@inbox.ru>
2024-01-31 22:03:17 +0600

    lab2 continue

---
+++ lab2_examples.py/classes and objects.py
@@ -0,0 +1,22 @@
+#1
+class MyClass:
+    x=5
+
+#2
+class MyClass:
+    x=5
+p1=MyClass()
+print(p1.x)
+
+#3
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
+#4
